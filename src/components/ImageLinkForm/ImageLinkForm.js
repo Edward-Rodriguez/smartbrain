56edb8f1908d54e150c75dc0d4fca87f5311086f
Edward Rodriguez <edwardR36@hotmail.com>
2020-08-02 17:39:21 -0400

    Add ImageLinkForm css Add center class for css Change gradient

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import './ImageLinkForm.css';
 
 const ImageLinkForm = () => {
   return (
     <div>
-      <p>
+      <p className='pa3'>
         {'This Magic Brain will detect faces in your pictures, give it a try'}
       </p>
-      <div>
-        <input type='text' />
-        <button>Detect</button>
+      <div className='center'>
+        <div className='center br3 pa4 shadow-5'>
+          <input className='br2 f4 pa3 w-70 center' type='tex' />
+          <button className='br2 w-30 grow f4 link ph3 pv2 dib white bg-light-purple pointer'>
+            Detect
+          </button>
+        </div>
       </div>
     </div>
   );

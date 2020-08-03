265c6eba8d077a52e1366d3de5781bb206712dbb
Edward Rodriguez <edwardR36@hotmail.com>
2020-08-03 09:40:05 -0400

    Add particles bg Add new Font Add rank

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import React from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+  },
+};
 
 function App() {
   return (
     <div className='App'>
+      <Particles className='particles' params={particlesOptions} />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* 
     <FaceRecognition />} */}
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -8,9 +8,9 @@ const ImageLinkForm = () => {
         {'This Magic Brain will detect faces in your pictures, give it a try'}
       </p>
       <div className='center'>
-        <div className='center br3 pa4 shadow-5'>
+        <div className='form center br3 pa4 shadow-5'>
           <input className='br2 f4 pa3 w-70 center' type='tex' />
-          <button className='br2 w-30 grow f4 link ph3 pv2 dib white bg-light-purple pointer'>
+          <button className='br2 w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>
             Detect
           </button>
         </div>
